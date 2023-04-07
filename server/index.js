d08293c2cf215adfd43efd6a23cad17437005747
Raúl Quimbaya <raulquimbaya4@gmail.com>
2023-04-07 11:18:53 -0500

    Renaming the "server" folder to "src"

--- server/index.js
+++
@@ -1,6 +0,0 @@
-const app = require('./app')
-const mongoConnection = require('./mongoConnection')
-const { PORT } = require('./config')
-
-mongoConnection()
-app.listen(PORT, () => console.log('Server is running'))
